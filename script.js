c4e3d3ba1132c03a995d8f777adedbe82a4a69a0
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-24 20:56:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+
+
+let sortedArr = arr.sort((a, b) => a - b);
+
+let sum = sortedArr[0] + sortedArr[1];
+
+for (let i = 2; i < sortedArr.length; i++) {
+    sum += sortedArr[i];
+}
+
+console.log(sortedArr); // [1, 2, 3, 4]
+console.log(sum);       // 10
 }
 
 module.exports=mincost;
