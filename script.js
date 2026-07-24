8f5f0b60241886ebe21d77e334014a41bf3dd3df
Kalpanab26 <89635401+Kalpanab26@users.noreply.github.com>
2026-07-24 21:16:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-function mincost(arr) {
-    let cost = 0;
+function mincost(arr)
+{
+ let cost = 0;
 
     while (arr.length > 1) {
         arr.sort((a, b) => a - b);
@@ -17,5 +18,4 @@ function mincost(arr) {
     return cost;
 }
 
-console.log(mincost([4, 3, 2, 6])); // 29
-console.log(mincost([1, 2, 3, 4, 5])); // 33
+module.exports=mincost;
